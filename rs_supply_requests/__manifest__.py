28087bbddfbd01c3a9fb60fccd351292fb282214
emremataraci <mtrcemre@gmail.com>
2023-03-02 10:10:23 +0300

    first commit

---
+++ rs_supply_requests/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "rs_supply_requests",
+
+    'summary': """
+        Sale  Order to Purchase Order""",
+
+    'description': """
+        SO2REQ > REQ2PO
+    """,
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'sale_management',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale_management', 'purchase','project'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/supply_requests_views.xml',
+        'views/sale_views.xml',
+    ],
+    "installable": True,
+    'license': 'LGPL-3',
+
+    
+}
